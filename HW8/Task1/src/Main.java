29d53ccc4d147d3c5c7cfce97215e52c560ff47c
Marlen Mels <marlenmels9@gmail.com>
2024-08-05 19:19:37 +0500

    All tasks were made by Marlen

--- HW8/Task1/src/Main.java
+++ HW8/Task1/src/Main.java
@@ -1,15 +1,36 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         //#1 Создать массив из 10 целых чисел. Значения могут быть любыми
         //todo
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
 
         //#2 Распечатать все значения массива начиная с 0 до последнего индексов.
         //todo
-
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
         //#3 Распечатать все значения массива с конца (начиная с последнего до 0 индексов).
         //todo
 
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
         //#4 *** Задание особой сложности. Развернуть массив(см конец урока).
         //todo
+        int temp = 0;
+        for (int i = 0; i < array.length / 2; i++) {
+            temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
     }
 }
